1b00f95a85d18b4b2579f96fdfd2f18eeadfb3fb
leminhkhanh81 <khanhle.t3@gmail.com>
2022-04-26 22:29:29 +0700

    Initial Commit

---
+++ GuessTheFlagSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlagSwiftUI
+//
+//  Created by Le Minh Khanh on 26/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlagSwiftUI/GuessTheFlagSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagSwiftUIApp.swift
+//  GuessTheFlagSwiftUI
+//
+//  Created by Le Minh Khanh on 26/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
